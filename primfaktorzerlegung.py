c2855833ca25f3055285d8c3734aa877ee019e8a
Manuel7568 <39556095+Manuel7568@users.noreply.github.com>
2018-05-23 15:51:29 +0200

    Add files via upload

---
+++ primfaktorzerlegung.py
@@ -0,0 +1,13 @@
+#Primfaktorzerlegung
+n = int(input("Bitte geben Sie Ihre Zahl ein."))
+
+primfac = []
+d = 2
+while d*d <= n:
+    while (n % d) == 0:
+        primfac.append(d)  # supposing you want multiple factors repeated
+        n //= d
+    d += 1
+if n > 1:
+    primfac.append(n)
+print(primfac)
